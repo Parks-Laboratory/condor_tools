ca628305ae766bd93046158d7716f4df1dcd0fca
ChrisGottsacker <atcgottsacker@gmail.com>
2017-03-18 19:18:08 -0500

    Display stats. for num. jobs completed, held, evicted

--- log_summary.sh
+++ log_summary.sh
@@ -1,10 +1,26 @@
+# Note: depends on Rscript (comes with R installation)
+
 if [ $# -ne 1 ]; then
 	echo Usage: $0 path/to/chtc/log_file
 	exit
 fi
 
-echo 'args=commandArgs(trailingOnly = TRUE); summary(scan(args[1])/as.numeric(args[2]))' > summarize.R
-echo ===============================================
+function printds {
+	echo -e $* \\n
+}
+
+function countJobs {
+	echo $(cat $1 | grep ^$2 | wc -l)
+}
+
+echo 'Number jobs completed/submitted:' $(countJobs $1 005) / $(countJobs $1 000)
+echo 'Number jobs put on hold/re-submitted:' $(countJobs $1 012) / $(countJobs $1 013)
+echo 'Number jobs evicted:' $(countJobs $1 004)
+
+echo 'args=commandArgs(trailingOnly = TRUE);
+	summary(scan(args[1])/as.numeric(args[2]))' > summarize.R
+
+printds \\n===============================================
 
 grep 'Disk (KB)' $1 > lines
 echo "Disk Usage (MB):"
@@ -15,7 +31,7 @@ echo "Disk Allocation (MB):"
 awk '{print $6}' lines > col
 Rscript summarize.R col 1000
 
-echo ===============================================
+printds \\n===============================================
 
 grep 'Memory (MB)' $1 > lines
 echo "Memory Usage (MB):"
@@ -26,13 +42,19 @@ echo "Memory Allocation (MB):"
 awk '{print $6}' lines > col
 Rscript summarize.R col 1
 
-echo ===============================================
+printds \\n===============================================
 
 echo 'Job duration (ignore dates)'
 grep 'Total Remote Usage' $1 > lines
 awk 'match($3, /([^,]*)/, a) {print a[1]}' lines > col
-echo 'args=commandArgs(trailingOnly = TRUE); summary(strptime(scan(args[1], what="character"), format="%H:%M:%S"))' > summarizeTime.R
+echo 'args=commandArgs(trailingOnly = TRUE); summary(strptime(scan(args[1],
+	what="character"), format="%H:%M:%S"))' > summarizeTime.R
 Rscript summarizeTime.R col
+
+printds \\n===============================================
+
+
+
 
 
 rm lines col summarize.R summarizeTime.R
