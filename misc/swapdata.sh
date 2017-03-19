37ade5db6eb104857252452e46b7fbf074b5dfb7
ChrisGottsacker <atcgottsacker@gmail.com>
2017-03-18 22:40:41 -0500

    Import script to switch b/n 2 versions of a dir.

---
+++ misc/swapdata.sh
@@ -0,0 +1,45 @@
+# Usage: ./swapdata.sh
+#
+# For a directory with unchanging name, quickly alternate between one set of
+# contents and another. Useful for testing using alternative data, and then
+# switching back to the actual data when testing is done.
+#
+# Given 2 versions of a directory (dir), backup the current version of dir
+# and replace the contents of dir with contents of the other version.
+
+
+dir="data"						# set this to name of main directory
+version1="1600"					# description of version 1 of dir
+version2="full"					# description of version 2 of dir
+
+function checkerror {
+	if [ ! $? -eq 0 ]; then
+		echo swap failed
+	fi
+}
+
+function swap {
+	outgoing=$1
+	incomming=$2
+
+	mv "$dir" "$outgoing"
+	checkerror
+	mv "$incomming" "$dir"
+	checkerror
+}
+
+if [ ! -d "$dir" ]; then
+	echo "Can't swap, \"$dir\" doesn't exist"
+	exit
+fi
+
+archive1=$dir"_"$version1
+archive2=$dir"_"$version2
+
+if [[ -d "$archive1" && ! -d "$archive2" ]]; then
+	swap $archive2 $archive1
+elif [[ -d "$archive2" && ! -d "$archive1" ]]; then
+	swap $archive1 $archive2
+else
+	echo swap failed, either \"$archive1\" or \"$archive2\" was missing or both were present
+fi
