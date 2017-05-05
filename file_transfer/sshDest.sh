570c1af261975245f582347a8d7d471cfdcf84fc
ChrisGottsacker <atcgottsacker@gmail.com>
2017-05-05 13:22:01 -0500

    Get username & simulate user hitting "enter"

--- file_transfer/sshDest.sh
+++ file_transfer/sshDest.sh
@@ -1,5 +1,6 @@
 echo UPDATE LIST OF AUTHORIZED KEYS
 cat id_rsa.pub >> ~/.ssh/authorized_keys
+
 echo MODIFY FILE PERMISSIONS
 chmod 0700 ~
 chmod 0700 ~/.ssh
--- file_transfer/sshLocal.sh
+++ file_transfer/sshLocal.sh
@@ -1,6 +1,18 @@
+# figure out username
+user=""
+if [ -z "$USER" ]; then
+	user=$USERNAME;
+else
+	user=$USER;
+fi
+
+hitenter=$'\n'
+
 echo MAKE NEW PUBLIC KEY
-ssh-keygen -t rsa
+ssh-keygen -t rsa <<< "${hitenter}${hitenter}${hitenter}"
+
 echo COPY PUBLIC KEY TO DESTINATION HOST
-scp ~/.ssh/id_rsa.pub sshDest.sh $USER@submit-3.chtc.wisc.edu:
+scp ~/.ssh/id_rsa.pub sshDest.sh $user@submit-3.chtc.wisc.edu:
+
 echo SSH TO DESTINATION HOST
-ssh $USER@submit-3.chtc.wisc.edu
+ssh $user@submit-3.chtc.wisc.edu
