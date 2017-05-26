036e9c9f4613f7996fd9ba01b62f3d1a800cfd5a
ChrisGottsacker <atcgottsacker@gmail.com>
2017-05-26 14:27:42 -0500

    Strip out epistasis-specific code

--- template_project/epistasis_submit.py
+++ template_project/epistasis_submit.py
@@ -17,37 +17,6 @@ from datetime import datetime
 import time
 import re
 
-
-
-debug = False
-
-# input/output folders
-root = os.path.split(os.path.realpath(sys.argv[0]))[0]
-dataLoc = os.path.join(root, 'data')
-condor_output_root = os.path.join(root, 'condor_out')
-job_output_root = os.path.join(root, 'results')
-
-# script locations
-prog_path = os.path.join(root, 'scripts')
-
-# filename formats
-FULL_DATASET = '.FULL'
-FILTERED_DATASET = '.FILTERED'
-
-class Tee(object):
-	def __init__(self, filename):
-		self.logfile = open(filename, 'a')
-
-	def send_output(self, s):
-		sys.stderr.write('%s\n' %s)
-		self.logfile.write('%s\n' %s)
-
-	def close(self):
-		self.logfile.close()
-
-def timestamp():
-	return datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M-%S')
-
 def run(params, flags):
 	os.chdir(root)
 	make_output_dirs(params)
@@ -55,7 +24,7 @@ def run(params, flags):
 	if params['jobs_to_rerun_filename']:
 		params['num_jobs'] = get_num_jobs_to_rerun(params)
 	else:
-		params['num_jobs'] = calculate_num_jobs(params, params['group_size'])
+		params['num_jobs'] = get_num_jobs_to_run(params)
 
 	write_submission_file(params, flags)
 	write_shell_script(params, flags)
@@ -63,30 +32,21 @@ def run(params, flags):
 	package_SQUID_files(params)
 	submit_jobs(params)
 
-def write_submission_files(params):
-	pass
-
-
 def write_submission_file(params, flags):
-	'''
-	Arguments:
-	offset -- used w/ DAGMan to split up jobs
-	'''
-
 	# submit and executable files
 	submit_template = textwrap.dedent(
-	'''	# Epistasis Submit File
+	'''	# template Submit File
 
 	universe = vanilla
 	requirements = (OpSysMajorVer == 6)
 
-	log = %(condor_output)s/epistasis_$(Cluster).log
-	error = %(condor_output)s/epistasis_$(Cluster)_$(Process).err
+	log = %(condor_output)s/template_$(Cluster).log
+	error = %(condor_output)s/template_$(Cluster)_$(Process).err
 
 	InitialDir = %(root)s/results/%(dataset)s
 	executable = %(root)s/%(executable_filename)s
-	arguments = $(Process) %(offset)s
-	output = %(condor_output)s/epistasis_$(Cluster)_$(Process).out
+	arguments = $(Process)
+	output = %(condor_output)s/template_$(Cluster)_$(Process).out
 
 	should_transfer_files = YES
 	when_to_transfer_output = ON_EXIT
@@ -120,7 +80,7 @@ def write_shell_script(params, flags):
 	'''	#!/bin/bash
 
 	cleanup(){
-		rm -r -f *.bed *.bim *.fam *.py *.pyc *.tar.gz *.txt python %(jobs_to_rerun_filename)s
+		rm -r -f *.py *.pyc *.tar.gz python %(jobs_to_rerun_filename)s
 	}
 
 	exit_on_failure(){
@@ -161,10 +121,8 @@ def write_shell_script(params, flags):
 	%(debug_shell)s
 
 	# run script
-	python epistasis_node.py %(dataset)s %(group_size)s %(job_number)s %(covFile)s %(debug)s %(species)s %(maxthreads)s %(feature_selection)s %(exclude)s %(condition)s
+	python template_node.py %(dataset)s %(job_number)s %(debug)s
 	exit_on_failure
-
-	echo "Process $1 (job %(job_number)s) generated gwas $(ls *gwas)"
 
 	cleanup
 
@@ -172,9 +130,12 @@ def write_shell_script(params, flags):
 	''')
 
 	if params['jobs_to_rerun_filename']:
+		# get job number from ($1 + 1)th line of rerun file, where $1 is the
+		# process number passed by CHTC to the job
 		params['job_number'] = '$(sed -n "$(( $1 + 1 ))"p %(jobs_to_rerun_filename)s)' % params
 	else:
-		params['job_number'] = '$(( $1 + $2 ))'
+		# $1 is the CHTC process number and $2 is the offset (intended for use with DAGMan)
+		params['job_number'] = '$1'
 
 	params['debug_shell'] = ''
 	if flags['debug']:
@@ -248,6 +209,9 @@ def submit_jobs(params):
 	print("Submitting Jobs to Cluster %s" % condor_cluster)
 	log.send_output("%s was sent to cluster %s at %s" % (params['dataset'], condor_cluster, timestamp()))
 
+def get_num_jobs_to_run(params):
+	return 1
+
 def get_num_jobs_to_rerun(params):
 	num_jobs = 0
 	with open(os.path.join(params['dataLoc'], params['jobs_to_rerun_filename'])) as f:
@@ -259,100 +223,33 @@ def get_num_jobs_to_rerun(params):
 			num_jobs += 1
 		return num_jobs
 
-def get_num_filtered_snps(params):
-	num_snps = 0
-	with open(os.path.join(params['dataLoc'], params['dataset']+FILTERED_DATASET+'.bim')) as f:
-		for line in f.readlines():
-			if line.strip():
-				num_snps += 1
-	return num_snps
-
-def calculate_num_jobs(params, group_size):
-	num_snps = get_num_filtered_snps(params)
-
-	num_groups = ceil(num_snps/group_size)
-
-	# for all groups A, B: num jobs comparing A to B, but ignoring the redundant
-	# jobs comparing B to A
-	num_AB_jobs = num_groups * (num_groups - 1) / 2
-
-	# for each comparison A to A, B to B that have not been done,
-	# 2 such comparisons are done per job
-	# (if num_groups is odd, then one job will only compare A to A, and not B to B)
-	num_AA_jobs = ceil(num_groups/2)
-
-	return int(num_AB_jobs + num_AA_jobs)
-
-
-def check_prefixes(dataloc, dataset):
-	'''
-	Looks in directory specified by dataloc, and ensures there are 2 sets of
-	binary files. Specifically, the following must exist:
-	*.FULL.bed, *.FULL.bim, *.FULL.fam
-	*.FILTERED.bed, *.FILTERED.bim, *.FILTERED.fam
-	where * is the same for all 6 files
-	'''
-	bin_files = [x for x in os.listdir(dataloc) if os.path.splitext(x)[1] in ['.bed', '.bim', '.fam']]
-	full_prefixes = [x for x in bin_files if os.path.splitext(x)[0] == dataset+FULL_DATASET]
-	filtered_prefixes = [x for x in bin_files if os.path.splitext(x)[0] == dataset+FILTERED_DATASET]
-
-	# check that prefixes match up and that each extension is included exactly twice
-	if not len(full_prefixes) == 3 or not len(filtered_prefixes) == 3:
-		sys.exit('ERROR: two sets of .bed/.bim/.fam files could not be found in "{}" with the prefix "{}"'.format(dataloc, dataset))
-
-
-
 if __name__ == '__main__':
-	import argparse
-	parser = argparse.ArgumentParser(description='''Runs FaST-LMM on datasets found in specified location (looks in %s by default). Each dataset should have PLINK-formatted genotype (*.tped, *.tfam) and alternate phenotype (*.pheno.txt) files.  Optional covariate files should be named with the same prefix as the other files and end in .covar.txt .
-''' % dataLoc)
-	#parser.set_usage('''%(prog)s [options] [dataset1] [dataset2] ... (runs all datasets if unspecified)
-	#PLINK-formatted genotype (*.tped, *.tfam) and alternate phenotype (*.pheno.txt) files should be placed in ''' + dataLoc)
-	parser.add_argument('dataset', metavar='dataset', nargs=1, type=str,
-			help='dataset(s) to process')
+	# input/output folders
+	root = os.path.split(os.path.realpath(sys.argv[0]))[0]
+	condor_output_root = os.path.join(root, 'condor_out')
+	job_output_root = os.path.join(root, 'results')
 
-	parser.add_argument('-l', '--list', dest='list_dataset',
-		help='lists datasets to process, does not do processing',
-		action='store_true')
+	# script locations
+	prog_path = os.path.join(root, 'scripts')
+
+
+	import argparse
+	parser = argparse.ArgumentParser(description='''Runs template on datasets found in specified location (looks in data/ by default).
+''')
+
+	parser.add_argument('dataset', help='dataset to process')
+
 	parser.add_argument('-d', '--datadir', dest='datadir',
 		help='specifies folder to search for raw data',
-		default=dataLoc, action='store')
-	parser.add_argument('-o', '--outputdir', dest='outputdir',
-		help='specifies output folder',
-		default=job_output_root, action='store')
-	parser.add_argument('-c', '--covar', dest='covar',
-		help='use covariate file', action='store_true')
-	parser.add_argument('-s', '--species', dest='species',
-		help='mouse or human',
-		default='mouse', action='store',
-		choices=['human', 'mouse', 'dog', 'horse', 'cow', 'sheep'])
+		default=os.path.join(root, 'data'), action='store')
 	parser.add_argument('-m', '--memory', dest='memory',
 		help='amount of RAM (in GB) requested per job',
 		default=8, action='store', type=int)
-	parser.add_argument('--maxthreads', dest='maxthreads',
-		help='maximum # of threads to use',
-		default=1, action='store', choices=range(1, 17), type=int)
 	parser.add_argument('-p', '--pools', dest='pools',
 		help='select from: chtc, osg, uw. E.g. -p chtc osg, to run on CHTC and OSG',
 		default=['chtc','osg','uw'], choices=['chtc','osg','uw'], nargs='*')
-	parser.add_argument('-f', '--feature-selection', dest='featsel',
-		help='perform feature selection', action='store_true')
-	parser.add_argument('-e', '--excludeByPosition', dest='exclude',
-		help='exclude SNPs within 2Mb of tested SNP from kinship matrix construction',
-		action='store_true')
-	parser.add_argument('-n', '--numeric_phenotype_id', dest='numeric',
-		help='convert phenotype names to numbers (for safety)',
-		nargs='?', default=0, const=1, type=int, action='store', choices=[0, 1, 2])
-	parser.add_argument('-q', '--quiet', dest='quiet',
-		help="suppress output", action='store_true')
 	parser.add_argument('--debug',
 		help="gather debugging information from run", action='store_true')
-	parser.add_argument('--tasks', dest='tasks', metavar='TASK', nargs='+',
-		help='run only specified sub-tasks (specify only one dataset when using this option)', type=int)
-	parser.add_argument('--condition', dest='condition',
-		help='condition on SNP {snp_id}', action='store', nargs=1)
-	parser.add_argument('-g', '--group_size', type=int,
-		help='number of snps in a group', action = 'store', default=1500)
 	parser.add_argument('--rerun', dest='jobs_to_rerun_filename', default='',
 		help='file name containing list of process/job numbers to run', action = 'store')
 
@@ -360,61 +257,23 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	dataset = args.dataset[0]
-
 	dataLoc = args.datadir
-	job_output_root = args.outputdir
-	list_data = args.list_dataset
-	covar = args.covar
 	memory = args.memory
 	pools = args.pools
-	numeric = args.numeric
-	species = args.species.lower()
-	maxthreads = args.maxthreads
-	featsel = args.featsel
-	exclude = args.exclude
 	debug = args.debug
-	tasks = args.tasks
-	condition = args.condition
-	group_size = args.group_size
 	jobs_to_rerun_filename = args.jobs_to_rerun_filename
-
-
-	if debug:
-		log = Tee('epistasis_pipeline-%s.log' % timestamp())
-	else:
-		log = Tee('/dev/null')
-
-	if tasks and len(datasets) > 1:
-		log.send_output('More than one dataset specified along with --tasks option; quitting')
-		log.close()
-		sys.exit(0)
-
-	log.send_output('Searching for raw data in %s' % dataLoc)
 
 	flags = {
 		'debug': debug,
 	}
 
 	params = {}
-
-	# initiate params
-	covFile = '%s.covar.txt' % dataset
-	params.update({ 'covFile': ''})
-	if covar and os.path.isfile(os.path.join(dataLoc,covFile)):
-		params.update({ 'covFile': '-c %s' % covFile})
-	elif covar:
-		log.send_output('Specified --covar but no covariate file exists; ignored')
-
-	if condition:
-		condition = condition[0]
-
 	# check_prefixes(dataLoc, dataset)
 	squid_archive = dataset + '.tar'
 	params.update({
 		'root': root,
 		'dataLoc': dataLoc,
 		'dataset': dataset,
-		'group_size': group_size,
 		'offset': 0,
 		'job_output': os.path.join(job_output_root, dataset),
 		'condor_output': os.path.join(condor_output_root, dataset),
@@ -423,26 +282,19 @@ if __name__ == '__main__':
 		'username': pwd.getpwuid(os.getuid()).pw_name,
 		'python_installation': 'python.tar.gz',
 		'atlas_installation': 'atlas.tar.gz',
-		'executable_filename' : 'epistasis_%s.sh' % dataset,
-		'submit_filename': 'epistasis_%s.sub' % dataset,
+		'executable_filename' : 'template_%s.sh' % dataset,
+		'submit_filename': 'template_%s.sub' % dataset,
 		'jobs_to_rerun_filename': jobs_to_rerun_filename,
 		'debug': ['', '--debug'][debug],
 		'prog_path':prog_path,
 		'timestamp':datetime.ctime(datetime.now()),
-		'species': '-s %s' % species,
 		'maxthreads':'--maxthreads %s' % maxthreads,
 		'feature_selection':['', '--feature-selection'][featsel],
 		'exclude':['', '--exclude'][exclude],
-		'condition': ['', '--condition %s' % condition][condition is not None],
 		'use_memory': memory,
 		'use_chtc': ['requirements = (Target.PoolName =!= "CHTC")', '']['chtc' in pools],
 		'use_osg': ['', '+wantGlidein = true']['osg' in pools],
 		'use_uw': ['', '+wantFlocking = true']['uw' in pools],
 	})
 
-
-	# maxthreads_option = ['', '-pe shared %s' % maxthreads][maxthreads > 1]
-
 	run(params, flags)
-
-	log.close()
