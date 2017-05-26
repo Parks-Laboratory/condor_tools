75593d1e4d4bd51f56a8a2f13a975a873388de2f
ChrisGottsacker <atcgottsacker@gmail.com>
2017-05-26 18:39:07 -0500

    Add node script, templated on epistasis_node.py

---
+++ template_project/scripts/template_node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from pprint import pprint
+
+if __name__ == '__main__':
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
+
+	parser.add_argument('dataset', help='dataset to run', action='store')
+	parser.add_argument('process_id', type=int, help= 'the job number to run', action='store')
+	parser.add_argument('--debug', dest='debug', help='log debugging output',
+	default=False, action='store_true')
+
+	args = parser.parse_args()
+
+	debug = args.debug
+	dataset = args.dataset
+	process_id = args.process_id
+
+	output_dir = root
+
+	if debug:
+		print('args:')
+		pprint(args)
